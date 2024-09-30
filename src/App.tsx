a3f8f73caddf2ad5d1cfd5546a7615d818d802c5
tochy97 <tochyegeonu97@gmail.com>
2024-09-29 19:47:11 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header/Header"
+import Footer from './components/Footer/Footer';
+import Content from './components/Content/Content';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
