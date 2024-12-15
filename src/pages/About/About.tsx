f77286197bd1160c60d7f0c52185f129d95f5ec3
tochy97 <tochyegeonu97@gmail.com>
2024-12-15 10:26:59 -0600

    More text edits in about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -25,20 +25,20 @@ export default function About(): ReactElement<any, any> {
       <article className={innerContainer + centerContainer + stack}>
         <h1 className={pageHeader + 'text-center'}>About</h1>
         <p className={innerText + 'indent-12 '}>
-          My name is Tochy and I am a Nigerian born American living in Texas. I am experienced building full-stack web applications, microservice based APIs, AI development and cloudbase ecosystems.
+          My name is Tochy, and I am a Nigerian born American living in Texas. I am experienced building full-stack web applications, microservice based APIs, AI development and cloud-base ecosystems.
         </p>
-        <p>
-          In my current role as a Software Developer at Argo, I have gained hands-on experience in designing and implementing features within an enteprise API, optimizing algorithms, and ensuring the scalability and reliability of microservices architectures.
+        <p className={innerText + 'indent-12 '}>
+          In my current role as a Software Developer at Argo, I have gained hands-on experience in designing and implementing features within an enterprise API, optimizing algorithms, and ensuring the scalability and reliability of microservices architectures. 
         </p>
-        <p>
-          I have worked extensively with Python, Java, Node.js, Go, SQL, NoSQL; Databases like Microsoft-SQL Server, MySQL, MongoDB, and PostgreSQL; Container management with Docker, Kubernetes and OpenShift.
+        <p className={innerText + 'indent-12 '}>
+          I also have professional experience as a network operation technician working at Provision Data Center. This role also required me to I set up local networks for data servers and crypto minors as well as performing daily checks and maintenance on these data servers and crypto minors. 
         </p>
         <aside className={innerText + 'indent-12 '}>
-          This website is a simple react app built to showcase my node packages from my organization<a  className={link} href="https://www.npmjs.com/org/egeonu"  target="_blank" rel="noopener noreferrer">&#9;@egeonu</a>. The website is hosted on Google Firebase and the source is built with React Typescript. I have
+          This website is a simple react app built to showcase my node packages from my organization<a className={link} href="https://www.npmjs.com/org/egeonu"  target="_blank" rel="noopener noreferrer">&#9;@egeonu</a>. The website is hosted on Google Firebase and I have
 continuous integration and deployments handled with Github Actions. - <a className={link} target="_blank" href='https://github.com/tochy97/personal'>Souce code. </a>
         </aside>
         <aside className={innerText + 'indent-12 '}>
-          The NyTimes page is something i wrote for fun. I am requesting a list of articles based on the day clicked in on the calender.
+          The NyTimes page is something i wrote for fun using my <a className={link} href="https://www.npmjs.com/package/@egeonu/calender-calculate"  target="_blank" rel="noopener noreferrer">&#9;@egeonu/calender</a>. I am requesting a list of articles based on the day clicked in on the calender.
           Enjoy the bubbles!
         </aside>
       </article>
