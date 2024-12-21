195bce63e7d28d153587b37c1905e81c310e32b0
tochy97 <tochyegeonu97@gmail.com>
2024-12-20 19:40:45 -0600

    Updated weather node package

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, RefObject } from "react";
 // @ts-ignore
-import Weather from '@egeonu/weather';
+import WeatherUI from '@egeonu/weather';
 
 import {
   centerContainer,
@@ -21,7 +21,7 @@ export default function About(): ReactElement<any, any> {
   return (
     <div className={stack + " place-items-center mb-[5em]"}>
       <h1 className={pageHeader + 'text-center'}>Grand Prairie, Tx Weather</h1>
-      <Weather longitude={-97.028336} latitude={32.679420} temperature_unit='c' wind_speed_unit='mph'/>
+      <WeatherUI longitude={-97.028336} latitude={32.679420} temperature_unit='f' wind_speed_unit='mph'/>
       <article className={innerContainer + centerContainer + stack}>
         <h1 className={pageHeader + 'text-center'}>About</h1>
         <p className={innerText + 'indent-12 '}>
